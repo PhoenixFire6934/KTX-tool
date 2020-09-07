9fa6f75ba0b490ef33b1319c16331abd60411577
Phoenix <63543285+PhoenixFire6879@users.noreply.github.com>
2020-09-07 13:27:42 +0300

    Update ktx2png.py

--- ktx2png.py
+++ ktx2png.py
@@ -1,16 +1,30 @@
 import os
+import platform
 
+def _(*args):
+    print('[Tool] ', end='')
+    for arg in args:
+        print(arg, end=' ')
+    print()
 
 if not os.path.isdir('ktx/'):
     os.mkdir('ktx')
-
 if not os.path.isdir('png/'):
     os.mkdir('png/')
 
 path = './ktx'
+system = platform.system()
 for file in os.listdir(path):
+    
     if file.endswith(".ktx"):
         filepath = 'ktx/' + file
         no_extension = file.split('\\')[-1].split('.')[0]
         opath = 'png/' +  no_extension +'.png'
-        os.system("PVRTexToolCLI.exe -i " + filepath + " -d " + opath + " -f r8g8b8a8 ")
+        
+        if system == 'Windows':
+            os.system("PVRTexToolCLI.exe -i " + filepath + " -d " + opath + " -f r8g8b8a8 ")
+        elif system == 'Linux':
+            os.system("chmod +x ./PVRTexToolCLI")
+            os.system("./PVRTexToolCLI -i " + filepath + " -d " + opath + " -f r8g8b8a8")
+        else:
+            _("Unsupported OS system")
